d90e0379252587d75d53a00c3bbcb25f46ae2b3c
Paweł Białaszczyk <pawel.bialaszczyk@itmagination.com>
2019-07-10 16:36:32 +0200

    Add templating engine

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,41 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate serde_derive;
 
 #[cfg(test)] mod tests;
 
+use rocket::Request;
+use rocket_contrib::templates::Template;
+
+#[derive(Serialize)]
+struct TemplateContext {
+    title: &'static str,
+    parent: &'static str,
+}
+
 #[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    Template::render("index", &TemplateContext {
+        title: "Tutaj",
+        parent: "layout",
+    })
+}
+
+#[catch(404)]
+fn not_found(req: &Request<'_>) -> Template {
+    let mut map = std::collections::HashMap::new();
+    map.insert("path", req.uri().path());
+    Template::render("error/404", &map)
+}
+
+fn setup() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/", routes![index])
+        .register(catchers![not_found])
+        .attach(Template::fairing())
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    setup().launch();
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,9 +1,29 @@
+use super::{setup, TemplateContext};
+
 use rocket::local::Client;
+use rocket::http::Status;
+use rocket_contrib::templates::Template;
 
 #[test]
-fn hello_world() {
-    let rocket = rocket::ignite().mount("/", routes![super::hello]);
-    let client = Client::new(rocket).unwrap();
+fn index() {
+    let client = Client::new(setup()).unwrap();
     let mut response = client.get("/").dispatch();
-    assert_eq!(response.body_string(), Some("Hello, world!".into()));
+    let context = TemplateContext {
+        title: "Tutaj",
+        parent: "layout",
+    };
+    let expected = Template::show(client.rocket(), "index", &context).unwrap();
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.body_string(), Some(expected));
+}
+
+#[test]
+fn not_found() {
+    let client = Client::new(setup()).unwrap();
+    let mut response = client.get("/lol").dispatch();
+    let mut map = std::collections::HashMap::new();
+    map.insert("path", "/lol");
+    let expected = Template::show(client.rocket(), "error/404", &map).unwrap();
+    assert_eq!(response.status(), Status::NotFound);
+    assert_eq!(response.body_string(), Some(expected));
 }
